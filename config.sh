c9534aa38bca037845e71663aa46d0ffbf4aa505
Mike Carlson <mikecarlson@users.noreply.github.com>
2015-01-19 01:00:09 -0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,15 +2,10 @@
 sudo apt-get update
 
 ### - Basic
-sudo apt-get -y install software-center
-sudo apt-get -y install synaptic
-sudo apt-get -y install software-properties-common
 sudo apt-get -y remove xscreensaver
-
-### Whisker menu
-sudo add-apt-repository -y ppa:gottcode/gcppa
+sudo add-apt-repository ppa:dockbar-main/ppa
 sudo apt-get update
-sudo apt-get -y install xfce4-whiskermenu-plugin
+sudo apt-get -y install xfce4-dockbarx-plugin
 
 ### - Numix
 sudo apt-add-repository -y ppa:numix/ppa
@@ -26,8 +21,7 @@ sudo apt-get -y install gimp
 sudo apt-get -y install inkscape
 sudo apt-get -y install scribus
 sudo apt-get -y install gparted
-sudo apt-get -y install xfce4-goodies
+sudo apt-get -y install terminator
 sudo apt-get -y install transmission
 sudo apt-get -y install torrent-search
-sudo apt-get -y install evince
 sudo apt-get -y install vim
