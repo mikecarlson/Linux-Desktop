a8159733ef54bdc4ccfbc6ac779dd960cde5da88
Mike Carlson <mikecarlson@users.noreply.github.com>
2015-04-08 21:29:46 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,24 +1,22 @@
 ### Setup
 sudo apt-get update
 
-### - Basic
-sudo apt-get -y remove xscreensaver
-sudo add-apt-repository ppa:dockbar-main/ppa
-sudo apt-get update
-sudo apt-get -y install xfce4-dockbarx-plugin
-
 ### - Numix
 sudo apt-add-repository -y ppa:numix/ppa
 sudo apt-get update
 sudo apt-get -y install numix-gtk-theme
 sudo apt-get -y install numix-icon-theme numix-icon-theme-circle
 
+### - Inkscape 0.9
+sudo apt-add-repository -y ppa:inkscape.dev/stable
+sudo apt-get update
+sudo apt-get -y instal inkscape
+
 ### - Repo apps
 sudo apt-get -y install docky
 sudo apt-get -y install ubuntu-restricted-extras
 sudo apt-get -y install libreoffice
 sudo apt-get -y install gimp
-sudo apt-get -y install inkscape
 sudo apt-get -y install scribus
 sudo apt-get -y install gparted
 sudo apt-get -y install terminator
