40d6391a99dc6ff2e3000b4cc245d5faab48d894
Mike Carlson <mikecarlson@users.noreply.github.com>
2015-08-19 16:33:01 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 ### - Add PPAs
 sudo add-apt-repository -y ppa:numix/ppa            # Numix icons and GTK
 sudo add-apt-repository -y ppa:inkscape.dev/stable  # Inkscape
+sudo add-apt-repository ppa:dockbar-main/ppa        # DocbarX
 
 sudo apt-get update
 
 ### - Install apps
 sudo apt-get -y install numix-gtk-theme numix-icon-theme numix-icon-theme-circle
 sudo apt-get -y install inkscape
+sudo apt-get -y install --no-install-recommends xfce4-dockbarx-plugin
 
 sudo apt-get -y install ubuntu-restricted-extras
 sudo apt-get -y install unity-tweak-tool
