74fc5bd61cb1a59b5e19d333c61afd6721f300a6
Mike Carlson <mikecarlson@users.noreply.github.com>
2016-01-06 22:01:56 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@ sudo apt-get -y install software-properties-common
 ### - Add PPAs
 sudo add-apt-repository -y ppa:numix/ppa            # Numix icons and GTK
 sudo add-apt-repository -y ppa:inkscape.dev/stable  # Inkscape
-sudo add-apt-repository -y ppa:dockbar-main/ppa        # DockbarX
+sudo add-apt-repository -y ppa:dockbar-main/ppa     # DockbarX
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo add-apt-repository -y ppa:libreoffice/ppa
+sudo add-apt-repository -y ppa:ricotz/docky         # Plank
 
 sudo apt-get update && sudo apt-get dist-upgrade
 
@@ -18,6 +19,7 @@ sudo apt-get -y install network-manager-openvpn network-manager-openvpn-gnome
 sudo apt-get -y install numix-gtk-theme numix-icon-theme numix-icon-theme-circle
 sudo apt-get -y install inkscape
 sudo apt-get -y install --no-install-recommends xfce4-dockbarx-plugin
+sudo apt-get -y install plank
 
 sudo apt-get -y install ubuntu-restricted-extras
 sudo apt-get -y install unity-tweak-tool
