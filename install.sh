49b1314b5ac4b26971f415b584f035d530395698
Mike Carlson <mikecarlson@users.noreply.github.com>
2016-02-18 14:44:11 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 ### Google Chrome
+echo 
 echo "Google Chrome ..."
 cd ~/Downloads
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt-get -y install -f
+sudo apt-get -f install
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get -f install
 sleep 10
 
 ### - For minimal crouton environment
@@ -17,16 +19,20 @@ sleep 10
 #sudo apt-get -y install --no-install-recommends xfce4-dockbarx-plugin
 
 ### - Add PPAs
-echo "PPAs"
+echo 
+echo "PPAs ..."
 sudo add-apt-repository -y ppa:inkscape.dev/stable  # Inkscape
 sudo add-apt-repository -y ppa:webupd8team/java     # (for Android Studio)
 sudo add-apt-repository -y ppa:mpstark/elementary-tweaks-daily #Elementary tweaks
 sleep 5
 
 ### - Update 
+echo 
+echo "Updates ..."
 sudo apt-get update
 
 ### - Install apps
+echo 
 echo "Main apps ..."
 sudo apt-get -y install elementary-tweaks
 sudo apt-get -y install oracle-java7-installer
@@ -44,6 +50,7 @@ sudo apt-get -y install ubuntu-restricted-extras
 sleep 10
 
 ### - LibreOffice
+echo 
 echo "LibreOffice 5.1"
 cd ~/Downloads
 wget http://download.documentfoundation.org/libreoffice/stable/5.1.0/deb/x86_64/LibreOffice_5.1.0_Linux_x86-64_deb.tar.gz
@@ -53,6 +60,7 @@ sudo dpkg -i *.deb
 sleep 10
 
 ### - Android Studio
+echo
 echo "Android Studio"
 cd ~/Downloads
 wget https://dl.google.com/dl/android/studio/ide-zips/1.5.1.0/android-studio-ide-141.2456560-linux.zip
@@ -65,13 +73,14 @@ cd android-studio/bin
 sleep 10
 
 ### - Mount points for NAS
+echo
+echo "Mount points for NAS"
 cd ~
 mkdir NAS
 cd NAS
 mkdir Google_Drive
 mkdir Utility
 mkdir Other_Media
-
 
 
 
