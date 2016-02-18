292ec4069e5414fe5ef84c282ca101d2d63b1040
Mike Carlson <mikecarlson@users.noreply.github.com>
2016-02-17 20:46:13 -0800

    Reworked to add major installations
    Added Google Chrome, LibreOffice, Android Studio

--- install.sh
+++ install.sh
@@ -1,33 +1,66 @@
+### Google Chrome
+echo "Google Chrome ..."
+cd ~/Downloads
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get -y install -f
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sleep 10
+
 ### - For minimal crouton environment
-sudo apt-get -y install software-properties-common
+#sudo apt-get -y install software-properties-common
+
+### XFCE
+#sudo add-apt-repository -y ppa:dockbar-main/ppa     # DockbarX
+#sudo add-apt-repository -y ppa:ricotz/docky         # Plank
+#sudo apt-get update
+#sudo apt-get -y install plank
+#sudo apt-get -y install --no-install-recommends xfce4-dockbarx-plugin
 
 ### - Add PPAs
-sudo add-apt-repository -y ppa:numix/ppa            # Numix icons and GTK
+echo "PPAs"
 sudo add-apt-repository -y ppa:inkscape.dev/stable  # Inkscape
-sudo add-apt-repository -y ppa:dockbar-main/ppa     # DockbarX
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo add-apt-repository -y ppa:ricotz/docky         # Plank
+sudo add-apt-repository -y ppa:webupd8team/java     # (for Android Studio)
+sudo add-apt-repository -y ppa:mpstark/elementary-tweaks-daily #Elementary tweaks
+sleep 5
 
-sudo apt-get update && sudo apt-get dist-upgrade
+### - Update 
+sudo apt-get update
 
 ### - Install apps
+echo "Main apps ..."
+sudo apt-get -y install elementary-tweaks
 sudo apt-get -y install oracle-java7-installer
 sudo apt-get -y install lib32z1 lib32ncurses5 lib32stdc++6
 sudo apt-get -y install cifs-utils
-sudo apt-get -y install network-manager-openvpn network-manager-openvpn-gnome
-sudo apt-get -y install numix-gtk-theme numix-icon-theme numix-icon-theme-circle
+#sudo apt-get -y install network-manager-openvpn network-manager-openvpn-gnome
 sudo apt-get -y install inkscape
-sudo apt-get -y install --no-install-recommends xfce4-dockbarx-plugin
-sudo apt-get -y install plank
-
-sudo apt-get -y install ubuntu-restricted-extras
-sudo apt-get -y install unity-tweak-tool
 sudo apt-get -y install gimp
 sudo apt-get -y install scribus
 sudo apt-get -y install gparted
 sudo apt-get -y install vim
 sudo apt-get -y install vlc
+sudo apt-get -y install ubuntu-restricted-extras
+sleep 10
 
+### - LibreOffice
+echo "LibreOffice 5.1"
 cd ~/Downloads
-wget http://download.documentfoundation.org/libreoffice/stable/5.0.4/deb/x86_64/LibreOffice_5.0.4_Linux_x86-64_deb.tar.gz
+wget http://download.documentfoundation.org/libreoffice/stable/5.1.0/deb/x86_64/LibreOffice_5.1.0_Linux_x86-64_deb.tar.gz
+tar -xvf LibreOffice_5.1.0_Linux_x86-64_deb.tar.gz
+cd LibreOffice_5.1.0.3_Linux_x86-64_deb/DEBS/
+sudo dpkg -i *.deb
+sleep 10
+
+### - Android Studio
+echo "Android Studio"
+cd ~/Downloads
+wget https://dl.google.com/dl/android/studio/ide-zips/1.5.1.0/android-studio-ide-141.2456560-linux.zip
+mv android-studio-ide-141.2456560-linux.zip ~
+cd ~
+unzip android-studio-ide-141.2456560-linux.zip
+rm android-studio-ide-141.2456560-linux.zip
+cd android-studio/bin
+./studio.sh
+
+
 
